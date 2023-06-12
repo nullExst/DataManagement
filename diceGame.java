dd441c03282cb7fc6a21064994e68f6c780f2980
h1x <fave7241@gmail.com>
2023-06-12 16:40:17 +0900

    Dice game: initial revision

---
+++ diceGame.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class diceGame {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+        System.out.println("Rolling dice...");
+        for (int i = 1; i <= 2; i++) {
+            int dice = rand.nextInt(6) + 1;
+            System.out.println("Dice " + i + ": " + dice);
+            sum += dice;
+        }
+        System.out.println("Total value: " + sum);
+    }
+}
